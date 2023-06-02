668c9c97a862fc5c9b53f060671f6755030be578
ThiagoAraujot <devthiagoaraujo@gmail.com>
2023-06-02 12:54:25 -0300

    atualizado

---
+++ aula13/repetições.js
@@ -0,0 +1,23 @@
+var c = 1
+var S = 0
+do { 
+    console.log(`${c}`)
+    c++
+    S = S + c
+} while (c <= 5)
+console.log(`A soma dos valores foi de ${S}`)
+
+
+/*
+var c = 1
+var S = 0
+while (c <= 5){
+    console.log(`${c}`)
+    c++
+    S = S + c
+    }
+    console.log(`A soma dos valores foi de: ${S}`)
+
+    */
+
+
